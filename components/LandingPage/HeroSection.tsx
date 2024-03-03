8cf6b25b84f3aba837845d0ec70f44e718c508b3
kingsleyibe <ibekingsley66@gmail.com>
2024-03-03 12:38:45 +0100

    Add text to the hero section component

--- components/LandingPage/HeroSection.tsx
+++ components/LandingPage/HeroSection.tsx
@@ -3,7 +3,7 @@
 const HeroSection = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <h2 className="text-2xl font-bold">Hero Section</h2>
+      <h2 className="text-2xl font-bold">Hero Section goes here</h2>
     </div>
   );
 };
