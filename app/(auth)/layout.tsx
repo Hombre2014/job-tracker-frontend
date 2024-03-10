d68efbf1ceeb757ac6d182d09cc72a7bc47518c8
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-03-10 12:14:37 +0100

    Implement Sign up form and related components

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -18,7 +18,9 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
             />
           </Link>
         </div>
-        {children}
+        <div className="flex flex-col items-center justify-center h-full w-full">
+          {children}
+        </div>
       </main>
     </>
   );
