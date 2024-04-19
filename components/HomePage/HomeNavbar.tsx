3bb124a6681006897c3eecb5f627d1cb0f8c13f7
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-04-19 17:40:12 +0200

    Add search box in the navbar

--- components/HomePage/HomeNavbar.tsx
+++ components/HomePage/HomeNavbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { RiMore2Fill } from 'react-icons/ri';
+import { RiMore2Fill, RiSearchLine } from 'react-icons/ri';
 
 import { ComboBox } from './ComboBox';
 
@@ -12,7 +12,14 @@ const HomeNavbar = () => {
           <RiMore2Fill />
           <ComboBox />
         </div>
-        <div>Filter</div>
+        <div className="flex items-center relative">
+          <RiSearchLine className="absolute left-1" />
+          <input
+            type="text"
+            placeholder="Filter"
+            className="rounded-md border border-slate-500 pl-6 w-20 h-9 border-dashed transition-all duration-300 ease-in-out"
+          />
+        </div>
       </div>
       <div>Main three buttons</div>
       <div>Add new stuff</div>
