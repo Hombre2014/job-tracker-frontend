54ef2f5239daecdf516c6e626740eb2e57c91502
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-08-09 16:45:34 +0200

    Add setStatusToIdle in useEffect in KanbanBoard.

--- app/(loggedin)/home/boards/[board_id]/board/page.tsx
+++ app/(loggedin)/home/boards/[board_id]/board/page.tsx
@@ -1,8 +1,18 @@
 'use client';
 
+import { useEffect } from 'react';
+
+import { useAppDispatch } from '@/redux/hooks';
+import { setStatusToIdle } from '@/redux/user/userSlice';
 import BoardColumns from '@/components/HomePage/Kanban/BoardColumns';
 
-const BoardKanban = () => {
+const KanbanBoard = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(setStatusToIdle());
+  }, [dispatch]);
+
   return (
     <div className="h-full">
       <BoardColumns />
@@ -10,4 +20,4 @@ const BoardKanban = () => {
   );
 };
 
-export default BoardKanban;
+export default KanbanBoard;
