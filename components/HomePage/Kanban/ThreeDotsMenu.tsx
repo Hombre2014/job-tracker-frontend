437d119619d1f91f29f69164227f5330e12953b9
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-08-19 10:41:09 +0200

    Fix the layout issue in ThreeDotsMenu file

--- components/HomePage/Kanban/ThreeDotsMenu.tsx
+++ components/HomePage/Kanban/ThreeDotsMenu.tsx
@@ -73,7 +73,7 @@ const ThreeDotsMenu = ({ columnOrder }: { columnOrder: number }) => {
       </div>
       <ul
         tabIndex={0}
-        className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+        className="dropdown-content !fixed menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
       >
         <li>
           <div className="flex !justify-between h-12 mb-1 p-4 w-full">
