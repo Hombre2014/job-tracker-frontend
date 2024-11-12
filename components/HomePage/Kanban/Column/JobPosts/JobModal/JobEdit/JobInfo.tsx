d417f9d6024da0385898d14714baae701af2f344
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2024-11-12 21:50:30 +0100

    Fix the issue with the duplicated code. Remove the unnessesary compnent.

--- components/HomePage/Kanban/Column/JobPosts/JobModal/JobEdit/JobInfo.tsx
+++ components/HomePage/Kanban/Column/JobPosts/JobModal/JobEdit/JobInfo.tsx
@@ -18,7 +18,6 @@ import {
   PopoverTrigger,
   PopoverAnchor,
 } from '@/components/ui/popover';
-import JobInfoContent from './JobInfoContent';
 
 const JobInfo = () => {
   const { job_id } = useParams();
@@ -91,183 +90,95 @@ const JobInfo = () => {
 
   return (
     <Card>
-      {firstVisit ? (
-        jobPostsStatus === 'succeeded' ? (
-          // <CardContent className="space-y-2">
-          //   <div className="flex gap-4 items-start">
-          //     <div className="flex w-2/3 pt-8">
-          //       <div className="flex flex-col w-full gap-4">
-          //         <div className="flex gap-2">
-          //           <InputElement
-          //             stylings="space-y-1 w-1/2"
-          //             labelName="Company"
-          //             id="company"
-          //             defaultValue={currentJobPost?.company.name}
-          //           />
-          //           <InputElement
-          //             stylings="space-y-1 w-1/2"
-          //             labelName="Job Title"
-          //             id="job-title"
-          //             defaultValue={currentJobPost?.title}
-          //           />
-          //         </div>
-          //         <div className="flex gap-2">
-          //           <InputElement
-          //             stylings="space-y-1 w-2/3"
-          //             labelName="Post URL"
-          //             id="post-url"
-          //             defaultValue=""
-          //             placeholderName="+ add URL"
-          //           />
-          //           <InputElement
-          //             stylings="space-y-1 w-1/3"
-          //             labelName="Salary"
-          //             id="salary"
-          //             sendData={handleSalaryChange}
-          //             value={currentJobPost?.salary}
-          //             placeholderName="+ add Salary"
-          //           />
-          //         </div>
-          //         <div className="flex gap-2">
-          //           <InputElement
-          //             stylings="space-y-1 w-2/3"
-          //             labelName="Location"
-          //             id="location"
-          //             defaultValue=""
-          //             placeholderName="+ add location"
-          //           />
-          //           <ColorPicker />
-          //         </div>
-          //         <TextEditor
-          //           initialText="Type your description here..."
-          //           title="Description"
-          //           buttonVisibility={false}
-          //         />
-          //       </div>
-          //     </div>
-          //     <div className="mt-8 flex flex-col w-1/3">
-          //       <span>Deadline</span>
-          //       <Popover>
-          //         <PopoverTrigger asChild>
-          //           <Button
-          //             variant={'outline'}
-          //             className={cn(
-          //               'min-w-full justify-start text-left font-normal mt-1',
-          //               !date && 'text-muted-foreground'
-          //             )}
-          //           >
-          //             {date ? (
-          //               format(date, 'PPP')
-          //             ) : (
-          //               <span>Deadline + set date</span>
-          //             )}
-          //           </Button>
-          //         </PopoverTrigger>
-          //         <PopoverContent className="w-auto p-0">
-          //           <Calendar
-          //             mode="single"
-          //             selected={date}
-          //             onSelect={handleSelect}
-          //             initialFocus
-          //           />
-          //         </PopoverContent>
-          //         <PopoverAnchor />
-          //       </Popover>
-          //     </div>
-          //   </div>
-          // </CardContent>
-          <JobInfoContent firstLoad={true} />
-        ) : (
-          <div className="min-h-[524px] flex flex-col justify-center">
-            <Loader title="Loading job post..." />
+      {!firstVisit || (jobPostsStatus === 'succeeded' && firstVisit) ? (
+        <CardContent className="space-y-2">
+          <div className="flex gap-4 items-start">
+            <div className="flex w-2/3 pt-8">
+              <div className="flex flex-col w-full gap-4">
+                <div className="flex gap-2">
+                  <InputElement
+                    stylings="space-y-1 w-1/2"
+                    labelName="Company"
+                    id="company"
+                    defaultValue={currentJobPost?.company.name}
+                  />
+                  <InputElement
+                    stylings="space-y-1 w-1/2"
+                    labelName="Job Title"
+                    id="job-title"
+                    defaultValue={currentJobPost?.title}
+                  />
+                </div>
+                <div className="flex gap-2">
+                  <InputElement
+                    stylings="space-y-1 w-2/3"
+                    labelName="Post URL"
+                    id="post-url"
+                    defaultValue=""
+                    placeholderName="+ add URL"
+                  />
+                  <InputElement
+                    stylings="space-y-1 w-1/3"
+                    labelName="Salary"
+                    id="salary"
+                    sendData={handleSalaryChange}
+                    value={currentJobPost?.salary}
+                    placeholderName="+ add Salary"
+                  />
+                </div>
+                <div className="flex gap-2">
+                  <InputElement
+                    stylings="space-y-1 w-2/3"
+                    labelName="Location"
+                    id="location"
+                    defaultValue=""
+                    placeholderName="+ add location"
+                  />
+                  <ColorPicker />
+                </div>
+                <TextEditor
+                  initialText="Type your description here..."
+                  title="Description"
+                  buttonVisibility={false}
+                />
+              </div>
+            </div>
+            <div className="mt-8 flex flex-col w-1/3">
+              <span>Deadline</span>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <Button
+                    variant={'outline'}
+                    className={cn(
+                      'min-w-full justify-start text-left font-normal mt-1',
+                      !date && 'text-muted-foreground'
+                    )}
+                  >
+                    {date ? (
+                      format(date, 'PPP')
+                    ) : (
+                      <span>Deadline + set date</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={handleSelect}
+                    initialFocus
+                  />
+                </PopoverContent>
+                <PopoverAnchor />
+              </Popover>
+            </div>
           </div>
-        )
-      ) : (
-        // <CardContent className="space-y-2">
-        //   <div className="flex gap-4 items-start">
-        //     <div className="flex w-2/3 pt-8">
-        //       <div className="flex flex-col w-full gap-4">
-        //         <div className="flex gap-2">
-        //           <InputElement
-        //             stylings="space-y-1 w-1/2"
-        //             labelName="Company"
-        //             id="company"
-        //             defaultValue={currentJobPost?.company.name}
-        //           />
-        //           <InputElement
-        //             stylings="space-y-1 w-1/2"
-        //             labelName="Job Title"
-        //             id="job-title"
-        //             defaultValue={currentJobPost?.title}
-        //           />
-        //         </div>
-        //         <div className="flex gap-2">
-        //           <InputElement
-        //             stylings="space-y-1 w-2/3"
-        //             labelName="Post URL"
-        //             id="post-url"
-        //             defaultValue=""
-        //             placeholderName="+ add URL"
-        //           />
-        //           <InputElement
-        //             stylings="space-y-1 w-1/3"
-        //             labelName="Salary"
-        //             id="salary"
-        //             sendData={handleSalaryChange}
-        //             value={currentJobPost?.salary}
-        //             placeholderName="+ add Salary"
-        //           />
-        //         </div>
-        //         <div className="flex gap-2">
-        //           <InputElement
-        //             stylings="space-y-1 w-2/3"
-        //             labelName="Location"
-        //             id="location"
-        //             defaultValue=""
-        //             placeholderName="+ add location"
-        //           />
-        //           <ColorPicker />
-        //         </div>
-        //         <TextEditor
-        //           initialText="Type your description here..."
-        //           title="Description"
-        //           buttonVisibility={false}
-        //         />
-        //       </div>
-        //     </div>
-        //     <div className="mt-8 flex flex-col w-1/3">
-        //       <span>Deadline</span>
-        //       <Popover>
-        //         <PopoverTrigger asChild>
-        //           <Button
-        //             variant={'outline'}
-        //             className={cn(
-        //               'min-w-full justify-start text-left font-normal mt-1',
-        //               !date && 'text-muted-foreground'
-        //             )}
-        //           >
-        //             {date ? (
-        //               format(date, 'PPP')
-        //             ) : (
-        //               <span>Deadline + set date</span>
-        //             )}
-        //           </Button>
-        //         </PopoverTrigger>
-        //         <PopoverContent className="w-auto p-0">
-        //           <Calendar
-        //             mode="single"
-        //             selected={date}
-        //             onSelect={handleSelect}
-        //             initialFocus
-        //           />
-        //         </PopoverContent>
-        //         <PopoverAnchor />
-        //       </Popover>
-        //     </div>
-        //   </div>
-        // </CardContent>
-        <JobInfoContent firstLoad={false} />
-      )}
+        </CardContent>
+      ) : jobPostsStatus !== 'succeeded' && firstVisit ? (
+        <div className="min-h-[524px] flex flex-col justify-center">
+          <Loader title="Loading job post..." />
+        </div>
+      ) : null}
     </Card>
   );
 };
