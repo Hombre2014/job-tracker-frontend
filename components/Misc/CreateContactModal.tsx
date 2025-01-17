b79a4bd1a53dc01aca26b86a49fc0df2282bec57
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-01-17 10:56:07 +0100

    Create a new component for creating a new contact modal.

---
+++ components/Misc/CreateContactModal.tsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
+import AlertDialogModal from '@/components/HomePage/Boards/AlertDialogModal';
+import CreateContactForm from '@/components/Forms/AddContact/CreateContactForm';
+
+interface CreateContactModalProps {
+  showButton: boolean;
+}
+
+const CreateContactModal = ({ showButton }: CreateContactModalProps) => {
+  const [, setIsMenuOpen] = useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
+  const [showContactModal, setShowContactModal] = useState(false);
+
+  const createContact = () => {
+    if (!isFormValid) return;
+
+    setShowContactModal(false);
+
+    // TODO: Implement contact creation
+  };
+
+  return (
+    <>
+      {showButton && (
+        <Button
+          variant="normal"
+          onClick={() => {
+            setShowContactModal(true);
+            setIsFormValid(false);
+          }}
+        >
+          + Contact
+        </Button>
+      )}
+      {showContactModal && (
+        <AlertDialogModal
+          buttonVariant="none"
+          buttonCancel="Discard"
+          buttonConfirm="Create"
+          open={showContactModal}
+          isFormValid={isFormValid}
+          contentWidth="!max-w-[900px]"
+          dialogTitle="Save New Contact"
+          actionFunction={createContact}
+          onOpenChange={(open) => {
+            setShowContactModal(open);
+            if (!open) setIsMenuOpen(false);
+          }}
+        >
+          <CreateContactForm onValidationChange={setIsFormValid} />
+        </AlertDialogModal>
+      )}
+    </>
+  );
+};
+
+export default CreateContactModal;
