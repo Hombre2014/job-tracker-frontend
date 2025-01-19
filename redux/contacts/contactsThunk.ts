b1e238281ab12a64bd6e2c185435710ace0a7a29
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-01-19 09:39:27 +0100

    Add Update and Delete action to the Thunk and Slice.

--- redux/contacts/contactsThunk.ts
+++ redux/contacts/contactsThunk.ts
@@ -52,3 +52,77 @@ export const createContact = createAsyncThunk(
     }
   }
 );
+
+export const updateContact = createAsyncThunk(
+  'contacts',
+  async (values: any, thunkAPI) => {
+    const {
+      id,
+      emails,
+      phones,
+      boardId,
+      comment,
+      lastName,
+      photoUrl,
+      jobTitle,
+      companies,
+      firstName,
+      gitHubUrl,
+      twitterUrl,
+      linkedinUrl,
+      accessToken,
+      facebookUrl,
+      companyLocation,
+    } = values;
+    const body = {
+      id,
+      emails,
+      phones,
+      boardId,
+      comment,
+      photoUrl,
+      lastName,
+      jobTitle,
+      firstName,
+      gitHubUrl,
+      companies,
+      twitterUrl,
+      linkedinUrl,
+      facebookUrl,
+      companyLocation,
+    };
+    try {
+      const res = await client.put(`/contacts`, body, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = res.data;
+      return data;
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(
+        err.response?.data || 'Error updating contact'
+      );
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts',
+  async (values: any, thunkAPI) => {
+    const { id, accessToken } = values;
+    try {
+      const res = await client.delete(`/contacts/${id}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = res.data;
+      return data;
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(
+        err.response?.data || 'Error deleting contact'
+      );
+    }
+  }
+);
