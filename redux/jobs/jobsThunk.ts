b0bca66052283565422fb124a9ba0e7494676d09
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-02-14 22:10:52 +0100

    Make the changes to check for existin compaqnies and if there is no to create a new one.

--- redux/jobs/jobsThunk.ts
+++ redux/jobs/jobsThunk.ts
@@ -5,13 +5,14 @@ import { defaultJobPostColor } from '@/data/constants';
 export const createJobPost = createAsyncThunk(
   'jobs/createJobPost',
   async (values: any, thunkAPI) => {
-    const { accessToken, title, companyName, columnId, status } = values;
+    const { accessToken, title, companyId, columnId, status } = values;
     const body = {
       title: title,
       columnId: columnId,
-      company: {
-        name: companyName,
-      },
+      // company: {
+      //   name: companyName,
+      // },
+      companyId: companyId,
       status: status,
       color: defaultJobPostColor,
       createdAt: new Date().toISOString(),
