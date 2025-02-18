3d82600843a29325f95e1aba1b42edd21b04f920
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-02-18 21:44:34 +0100

    Add updateCompanu thunk and slice and implement the form component for edit the company.

--- components/Forms/EditCompanyForm.tsx
+++ components/Forms/EditCompanyForm.tsx
@@ -2,15 +2,165 @@
 
 import * as z from 'zod';
 import { useForm } from 'react-hook-form';
-import { Input } from '@/components/ui/input';
+import { useParams } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
 
+import { Input } from '@/components/ui/input';
 import { EditCompanySchema } from '@/schemas';
+import { Button } from '@/components/ui/button';
+import { Textarea } from '@/components/ui/textarea';
+import { FormError } from '@/components/Forms/form-error';
+import { FormSuccess } from '@/components/Forms/form-success';
+import { useAppDispatch, useAppSelector } from '@/redux/hooks';
+import { updateCompany } from '@/redux/companies/companiesThunk';
+import {
+  Form,
+  FormItem,
+  FormField,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from '@/components/ui/form';
 
-const EditCompanyForm = () => {
+interface EditCompanyFormProps {
+  schema: z.Schema;
+  buttonText: string;
+  onClose: () => void;
+  errorMessage?: string;
+  successMessage?: string;
+}
+
+const EditCompanyForm = ({
+  schema,
+  onClose,
+  buttonText,
+  errorMessage,
+  successMessage,
+}: EditCompanyFormProps) => {
+  const dispatch = useAppDispatch();
+  const { job_id } = useParams();
+
+  const { jobPosts } = useAppSelector((state) => state.jobs);
+  const currentJobPost = Array.isArray(jobPosts)
+    ? jobPosts.find((jobPost) => jobPost.id === job_id)
+    : null;
+
+  const onSubmit = (data: z.infer<typeof EditCompanySchema>) => {
+    const accessToken = localStorage.getItem('accessToken');
+    dispatch(
+      updateCompany({
+        ...data,
+        accessToken,
+        companyId: currentJobPost?.company?.id,
+      })
+    );
+
+    onClose();
+  };
+
+  const form = useForm<z.infer<typeof EditCompanySchema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      url: currentJobPost?.company?.url || '',
+      name: currentJobPost?.company?.name || '',
+      industry: currentJobPost?.company?.industry || '',
+      description: currentJobPost?.company?.description || '',
+    },
+  });
+
+  const {
+    handleSubmit,
+    formState: { errors },
+  } = form;
+
   return (
     <div>
-      <h1>Form</h1>
+      <Form {...form}>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Company Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Company Description"
+                    {...field}
+                    rows={4}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="industry"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Industry</FormLabel>
+                <FormControl>
+                  <Input placeholder="Company Industry" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>URL</FormLabel>
+                <FormControl>
+                  <Input placeholder="Company URL" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex gap-4 items-center mt-4">
+            <Button type="submit" variant="normal">
+              {buttonText}
+            </Button>
+            <Button
+              type="button"
+              variant="destructive"
+              onClick={() => form.reset()}
+            >
+              Discard
+            </Button>
+          </div>
+
+          {Object.keys(errors).length > 0 && (
+            <FormError
+              message={Object.values(errors)
+                .map((error) => error?.message)
+                .join(', ')}
+            />
+          )}
+          {successMessage && <FormSuccess message={successMessage} />}
+          {errorMessage && <FormError message={errorMessage} />}
+        </form>
+      </Form>
     </div>
   );
 };
--- components/HomePage/Kanban/Column/JobPosts/JobModal/JobCompany/Company.tsx
+++ components/HomePage/Kanban/Column/JobPosts/JobModal/JobCompany/Company.tsx
@@ -1,61 +1,76 @@
-import { useParams } from 'next/navigation';
-
-import { useAppDispatch } from '@/redux/hooks';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription } from '@/components/ui/card';
 import Link from 'next/link';
+import { useState } from 'react';
+
+import { EditCompanySchema } from '@/schemas';
+import { Button } from '@/components/ui/button';
+import EditCompanyForm from '@/components/Forms/EditCompanyForm';
+import { Card, CardContent, CardDescription } from '@/components/ui/card';
 
 const Company = () => {
+  const [showModal, setShowModal] = useState(false);
   const currentJobPost: JobApplication = JSON.parse(
     localStorage.getItem('currentJobPost') || '{}'
   );
 
   const editCompanyDetails = () => {
-    console.log('edit company details');
+    setShowModal(true);
   };
 
   return (
-    <div className="flex flex-col gap-4 w-full">
-      <div className="flex justify-end mb-4">
-        <Button variant="normal" onClick={editCompanyDetails}>
-          Edit Company
-        </Button>
-      </div>
-      <div className="flex gap-4">
-        <div className="flex flex-col gap-4 w-2/3">
-          <h2 className="text-2xl">{currentJobPost?.company.name}</h2>
-          <p className="text-muted-foreground">
-            {currentJobPost?.company.description}
-          </p>
-
-          <Link
-            target="_blank"
-            rel="noreferrer noopener"
-            href={currentJobPost?.company.url || '#'}
-            onClick={(e) => {
-              if (!currentJobPost?.company.url) {
-                e.preventDefault();
-              }
-            }}
-          >
-            <Button variant="normal" disabled={!currentJobPost?.company.url}>
-              Visit Website
+    <>
+      {showModal ? (
+        <EditCompanyForm
+          buttonText="Save"
+          schema={EditCompanySchema}
+          onClose={() => setShowModal(false)}
+        />
+      ) : (
+        <div className="flex flex-col gap-4 w-full">
+          <div className="flex justify-end mb-4">
+            <Button variant="normal" onClick={editCompanyDetails}>
+              Edit Company
             </Button>
-          </Link>
+          </div>
+          <div className="flex gap-4">
+            <div className="flex flex-col gap-4 w-2/3">
+              <h2 className="text-2xl">{currentJobPost?.company.name}</h2>
+              <p className="text-muted-foreground">
+                {currentJobPost?.company.description}
+              </p>
+
+              <Link
+                target="_blank"
+                rel="noreferrer noopener"
+                href={currentJobPost?.company.url || '#'}
+                onClick={(e) => {
+                  if (!currentJobPost?.company.url) {
+                    e.preventDefault();
+                  }
+                }}
+              >
+                <Button
+                  variant="normal"
+                  disabled={!currentJobPost?.company.url}
+                >
+                  Visit Website
+                </Button>
+              </Link>
+            </div>
+            <Card className="w-1/3">
+              <CardContent className="flex flex-col gap-4">
+                <h3 className="mt-4">Website</h3>
+                <CardDescription>{currentJobPost?.company.url}</CardDescription>
+                <hr className="" />
+                <h3>Industry</h3>
+                <CardDescription>
+                  {currentJobPost?.company.industry}
+                </CardDescription>
+              </CardContent>
+            </Card>
+          </div>
         </div>
-        <Card className="w-1/3">
-          <CardContent className="flex flex-col gap-4">
-            <h3 className="mt-4">Website</h3>
-            <CardDescription>{currentJobPost?.company.url}</CardDescription>
-            <hr className="" />
-            <h3>Industry</h3>
-            <CardDescription>
-              {currentJobPost?.company.industry}
-            </CardDescription>
-          </CardContent>
-        </Card>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
