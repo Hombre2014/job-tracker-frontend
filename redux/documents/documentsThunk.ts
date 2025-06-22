98d4806a69ad5fbcbe9e6845c022c7fb708bc247
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-06-22 17:03:32 +0200

    feat: add 5 thunks for documents

---
+++ redux/documents/documentsThunk.ts
@@ -0,0 +1,148 @@
+import client from '@/api/client';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getDocument = createAsyncThunk(
+  'documents/getDocument',
+  async (values: any, thunkAPI) => {
+    const { accessToken, documentId } = values;
+    try {
+      const res = await client.get(`/documents/${documentId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = res.data;
+      return data;
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(
+        err.response?.data || 'Error getting document'
+      );
+    }
+  }
+);
+
+export const uploadDocument = createAsyncThunk(
+  'documents/uploadDocument',
+  async (values: any, thunkAPI) => {
+    const {
+      file,
+      title,
+      boardId,
+      category,
+      description,
+      accessToken,
+    }: {
+      file: File;
+      title: string;
+      boardId: string;
+      category: string;
+      accessToken: string;
+      description: string;
+    } = values;
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('title', title);
+      formData.append('boardId', boardId);
+      formData.append('category', category);
+      formData.append('description', description);
+
+      const res = await client.post(`/documents`, formData, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      const data = res.data;
+      return data;
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(
+        err.response?.data || 'Error uploading document'
+      );
+    }
+  }
+);
+
+export const attachDocumentToJobApplication = createAsyncThunk(
+  'documents/attachDocumentToJobApplication',
+  async (values: any, thunkAPI) => {
+    const {
+      jobId,
+      documentId,
+      accessToken,
+    }: {
+      jobId: string;
+      documentId: string;
+      accessToken: string;
+    } = values;
+    try {
+      const res = await client.post(
+        `/documents/${documentId}/job-applications/${jobId}/attach`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const data = res.data;
+      return data;
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(
+        err.response?.data || 'Error attaching document to job application'
+      );
+    }
+  }
+);
+
+export const detachDocumentFromJobApplication = createAsyncThunk(
+  'documents/detachDocumentFromJobApplication',
+  async (values: any, thunkAPI) => {
+    const {
+      jobId,
+      documentId,
+      accessToken,
+    }: {
+      jobId: string;
+      documentId: string;
+      accessToken: string;
+    } = values;
+    try {
+      const res = await client.post(
+        `/documents/${documentId}/job-applications/${jobId}/detach`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const data = res.data;
+      return data;
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(
+        err.response?.data || 'Error detaching document from job application'
+      );
+    }
+  }
+);
+
+export const deleteDocument = createAsyncThunk(
+  'documents/deleteDocument',
+  async (values: any, thunkAPI) => {
+    const { documentId, accessToken } = values;
+    try {
+      const res = await client.delete(`/documents/${documentId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      const data = res.data;
+      return data;
+    } catch (err: any) {
+      return thunkAPI.rejectWithValue(
+        err.response?.data || 'Error deleting document'
+      );
+    }
+  }
+);
