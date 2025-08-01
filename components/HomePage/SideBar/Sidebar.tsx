d9929a8649bf8cdf53e8758f6f93dab716cfb912
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-08-01 11:52:29 +0200

    feat: add mode toggle button in user's side bar

--- components/HomePage/SideBar/Sidebar.tsx
+++ components/HomePage/SideBar/Sidebar.tsx
@@ -7,6 +7,9 @@ import JobBoard from './JobBoard';
 import UserPanel from './UserPanel';
 import JobTrackers from './JobTrackers';
 import SideBarMenuItem from './SideBarMenuItem';
+import { ModeToggle } from '@/components/Themes/mode-toggle';
+
+
 
 const Sidebar = () => {
   return (
@@ -19,6 +22,12 @@ const Sidebar = () => {
         <div className="border-b border-slate-200 h-auto pb-6">
           <JobTrackers />
           <JobBoard />
+        </div>
+      </div>
+      <div className="border-t border-slate-200 p-2">
+        <div className="flex items-center gap-3">
+          <ModeToggle />
+          <span className="text-sm">Theme</span>
         </div>
       </div>
       <UserPanel />
