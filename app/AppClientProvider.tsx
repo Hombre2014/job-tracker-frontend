53edc7eff298dfd315fb4f4afb34f3ca7db39781
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2026-01-24 21:48:35 +0100

    fix: minor warnings

--- app/AppClientProvider.tsx
+++ app/AppClientProvider.tsx
@@ -4,8 +4,8 @@ import AuthProvider from '@/components/auth/AuthProvider';
 
 export default function AppClientProviders({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return <AuthProvider>{children}</AuthProvider>;
 }
